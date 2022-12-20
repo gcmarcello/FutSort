9fe64173e2172f61bb6c8f23584448cdfd9575bc
gcmarcello <marcellogcoelho@outlook.com>
2022-12-20 05:49:05 -0300

    force https solution

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
 /* Middlewares */
+app.use(requireHTTPS);
 app.use(express.json());
 app.use(cors());
 
